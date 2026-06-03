4972c29495de8b5d7115fc03c274eeb56bdd73ca
yamamoto.kiyomi <yamamoto.kiyomi.c8s@eng.nipponsteel.com>
2026-06-03 15:02:42 +0900

    Reenable fibonacci unit test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -58,13 +58,4 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
 
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
